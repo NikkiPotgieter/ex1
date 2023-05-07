808e78c53b8f89c85881aae62cc6b2bd61355ad4
NikkiPotgieter <125267113+NikkiPotgieter@users.noreply.github.com>
2023-05-07 13:26:57 +0200

    Javascript
    Add JavaScript to index page

---
+++ js/index.js
@@ -0,0 +1,49 @@
+var name = "Nikki"
+var surname = "Potgieter"
+var age = 19
+const subject = "Development"
+
+var message = "Hello, my name is " + name + " " + surname + " I am " + age + " years old and I am studying " + subject;
+
+console.log(message)
+
+let numOne = 0
+let numTwo = 1
+let numThree = 1
+let numFour = 0
+let answer = 0
+
+answer = numOne + numTwo / numThree * numFour
+
+console.log(answer)
+
+const timmy = 10
+const susan = 25
+
+const timmyTax = timmy * 0.15
+const susanTax = susan * 0.26
+
+const value = timmyTax + susanTax
+
+console.log("The Tax Man will claim R" + value + " from Timmy and Susan")
+
+const johnScore = 82
+const timScore = 75
+const alfieScore = 60
+const gwenScore = 67
+
+const totalScore = johnScore + timScore + alfieScore + gwenScore
+const numStudents = 4
+const averageScore = totalScore / numStudents
+
+console.log("The student's average is:" + " " + averageScore + "/" + 100)
+
+const johnNewScore = johnScore + 7
+const timNewScore = timScore + 7
+const alfieNewScore = alfieScore + 7
+const gwenNewScore = gwenScore + 7
+
+const totalNewScore = johnNewScore + timNewScore + alfieNewScore + gwenNewScore
+const averageNewScore = totalNewScore / numStudents
+
+console.log("The student's updated average is:" + " " + averageNewScore + "/" + 100)
